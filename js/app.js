61cf7060be04f24b956f02502233c35bb4b66a1e
zaturnvalley <zacharycmason@gmail.com>
2016-09-15 16:50:51 -0700

    added angular app & controller, used ng-repeat to create list

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 /* setup your angular app here */
+var fruitVeggieApp = angular.module('FruitVeggieApp', []);
 
 // debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
 console.log('Fruit count', fruits.length);
 console.log('Veggie count', vegetables.length);
+
+
+fruitVeggieApp.controller('FruitVeggieCtrl', ['$scope', function($scope) {
+  $scope.fruitVegList = fruits.concat(vegetables);
+
+}])
